29e0b8134ad9dba7483cca81e253fdb6738b659f
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-04-09 12:32:55 -0300

    Upload Desafio 1

---
+++ desafio-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <p>Meu parágrafo em React.</p>
+    </div>
+  );
+}
+
+export default App;
