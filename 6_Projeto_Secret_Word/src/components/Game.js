d19ea4113d8769f7180231882fd49a0d53d45cf0
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-05-08 22:47:29 -0300

    Commit Módulo 6

---
+++ 6_Projeto_Secret_Word/src/components/Game.js
@@ -0,0 +1,55 @@
+import './Game.css'
+
+import { useState, useRef } from 'react'
+
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
+    const [letter, setLetter] = useState("")
+    const letterInputRef = useRef(null)
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        verifyLetter(letter);
+
+        setLetter("")
+
+        letterInputRef.current.focus()
+    }
+  return (
+    <div>
+        <div className="game">
+            <p className="points">
+                <span>Pontuação: {score}</span>
+            </p>
+        </div>
+        <h1>Adivinhe a palavra.</h1>
+        <h3 className="tip">
+            Dica sobre a palavra: <span>{pickedCategory}</span>
+        </h3>
+        {guesses===1 ? <p>Você ainda tem {guesses} tentativa.</p> : <p>Você ainda tem {guesses} tentativas.</p>}
+        <div className="wordContainer">
+            {letters.map((letter, i) => (
+                guessedLetters.includes(letter) ? (
+                    <span key={i} className="letter">{letter}</span>
+                ) : (
+                    <span key={i} className="blankSquare"></span>
+                )
+            ))}
+        </div>
+        <div className="letterContainer">
+            <p>Tente adivinhar uma letra da palavra:</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="letter" maxLength="1" required onChange={(e)=> setLetter(e.target.value)} value = {letter} ref={letterInputRef}/>
+                <button>Jogar</button>
+            </form>
+        </div>
+        <div className="wrongLetters">
+            <p>Letras já utilizadas:</p>
+            {wrongLetters.map((letter, i) => (
+                wrongLetters.length < 1 ? <span key={i}>{letter},</span> : <span key={i}>{letter}, </span>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Game
