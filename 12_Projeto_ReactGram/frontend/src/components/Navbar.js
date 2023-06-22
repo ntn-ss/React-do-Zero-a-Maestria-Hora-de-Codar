5b947f060f96fcb90f507798b89f040bc1d6f8aa
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-06-22 14:36:45 -0300

    Upload Parte 4 Módulo 12

--- 12_Projeto_ReactGram/frontend/src/components/Navbar.js
+++ 12_Projeto_ReactGram/frontend/src/components/Navbar.js
@@ -4,8 +4,30 @@ import "./Navbar.css"
 import {NavLink, Link} from 'react-router-dom'
 import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from 'react-icons/bs'
 
+// Hooks
+import { useState } from "react"
+import { useAuth } from '../hooks/useAuth'
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+// Hooks
+import { logout, reset } from '../slices/authSlice'
+
 const Navbar = () => {
-  return (
+    const { auth } = useAuth()
+    const { user } = useSelector((state)=>state.auth)
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+
+        navigate("/login")
+    }
+  
+    return (
     <nav id="nav">
         <Link to="/">ReactGram</Link>
         <form id="search-form">
@@ -13,21 +35,45 @@ const Navbar = () => {
             <input type="text" placeholder="Pesquisar"/>
         </form>
         <ul id="nav-links">
-            <li>
-                <NavLink to="/">
-                    <BsHouseDoorFill />
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/login">
-                    Entrar
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/register">
-                    Cadastrar
-                </NavLink>
-            </li>
+            {
+                auth ? (
+                    <>
+                        <li>
+                            <NavLink to="/">
+                                <BsHouseDoorFill />
+                            </NavLink>
+                        </li>
+                        {user && (
+                            <li>
+                                <NavLink to={`/users/${user._id}`}>
+                                    <BsFillCameraFill />
+                                </NavLink>
+                            </li>
+                        )}
+                        <li>
+                            <NavLink to="/profile">
+                                <BsFillPersonFill />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <span onClick={handleLogout}>Sair</span>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <NavLink to="/login">
+                                Entrar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register">
+                                Cadastrar
+                            </NavLink>
+                        </li>
+                    </>
+                )
+            }
         </ul>
     </nav>
   )
--- 12_Projeto_ReactGram/frontend/src/pages/Auth/Register.js
+++ 12_Projeto_ReactGram/frontend/src/pages/Auth/Register.js
@@ -2,12 +2,20 @@ import "./Auth.css"
 
 // Components
 import {Link} from 'react-router-dom'
-
+import Message from '../../components/Message'
 
 // Hooks
 import { useState, useEffect } from "react"
+import {useSelector, useDispatch} from 'react-redux'
+
+// Redux
+import {register, reset} from '../../slices/authSlice'
 
 const Register = () => {
+
+  const dispatch = useDispatch()
+  const {loading,error} = useSelector((state)=>state.auth)
+
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -23,7 +31,14 @@ const Register = () => {
       confirmPassword
     }
     console.log(user);
+    dispatch(register(user))
   }
+
+  // clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <div id="register">
       <h2>ReactGram</h2>
@@ -33,7 +48,9 @@ const Register = () => {
         <input type="email" placeholder="E-mail" onChange={(e)=>setEmail(e.target.value)} value={email || ""} />
         <input type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)} value={password || ""} />
         <input type="password" placeholder="Confirme a senha" onChange={(e)=>setConfirmPassword(e.target.value)} value={confirmPassword || ""}/>
-        <input type="submit" value="Cadastrar" />
+        {!loading && <input type="submit" value="Cadastrar" /> }
+        {loading && <input type="submit" value="Aguarde..." disabled /> }
+        {error && <Message msg={error} type="error" />}
       </form>
       <p>Já tem uma conta? <Link to="/login">Clique aqui</Link>.</p>
     </div>
